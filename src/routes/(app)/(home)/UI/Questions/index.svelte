0c2e4909e8cb0197859b3478444630c683069ecd
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-09-09 13:57:42 +0300

    c

---
+++ src/routes/(app)/(home)/UI/Questions/index.svelte
@@ -0,0 +1,74 @@
+<div class="bg-gray-50">
+	<div class="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8 lg:py-40">
+		<h2 class="text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+			Часто задаваемые вопросы
+		</h2>
+		<dl class="mt-20 divide-y divide-gray-900/10">
+			<div class="py-8 first:pt-0 last:pb-0 lg:grid lg:grid-cols-12 lg:gap-8">
+				<dt class="text-base/7 font-semibold text-gray-900 lg:col-span-5">Как это работает?</dt>
+				<dd class="mt-4 lg:col-span-7 lg:mt-0">
+					<p class="text-base/7 text-gray-600">
+						Зачастую у вас может быть клиент, который по объективным причинам не может приобрести
+						необходимый товар или услугу у вас. В такой ситуации вы можете с его согласия передать
+						контакты нашим партнёрам через специальную анонимную форму. Мы гарантируем
+						конфиденциальность и защиту ваших данных. За успешную сделку вы получаете вознаграждение
+						— до 5% от её стоимости.
+					</p>
+				</dd>
+			</div>
+			<div class="py-8 first:pt-0 last:pb-0 lg:grid lg:grid-cols-12 lg:gap-8">
+				<dt class="text-base/7 font-semibold text-gray-900 lg:col-span-5">А вдруг меня уволят?</dt>
+				<dd class="mt-4 lg:col-span-7 lg:mt-0">
+					<p class="text-base/7 text-gray-600">
+						Мы предполагаем, что передача вами данных клиента осуществляется без возникновения
+						конфликта интересов с вашей компанией. При этом мы строго соблюдаем требования
+						Федерального закона №152-ФЗ и не передаём ваши контактные данные и информацию о сделках
+						третьим лицам. Сама регистрация в нашем сервисе является конфиденциальной, поэтому наши
+						партнёры не имеют доступа к информации о вас.
+					</p>
+				</dd>
+			</div>
+			<div class="py-8 first:pt-0 last:pb-0 lg:grid lg:grid-cols-12 lg:gap-8">
+				<dt class="text-base/7 font-semibold text-gray-900 lg:col-span-5">Как я получу бонусы?</dt>
+				<dd class="mt-4 lg:col-span-7 lg:mt-0">
+					<p class="text-base/7 text-gray-600">
+						После регистрации вы укажите удобный для вас способ получения бонуса. Подробности будут
+						в вашем личном кабинете.
+					</p>
+				</dd>
+			</div>
+			<div class="py-8 first:pt-0 last:pb-0 lg:grid lg:grid-cols-12 lg:gap-8">
+				<dt class="text-base/7 font-semibold text-gray-900 lg:col-span-5">А что с налоговой?</dt>
+				<dd class="mt-4 lg:col-span-7 lg:mt-0">
+					<p class="text-base/7 text-gray-600">
+						Мы не являемся вашим налоговым агентом и вы самостоятельно решаете судьбу дохода и
+						уплаченных налогов. Для полной легальности оформитесь как самозанятый или ИП и уплатите
+						символический налог.
+					</p>
+				</dd>
+			</div>
+			<div class="py-8 first:pt-0 last:pb-0 lg:grid lg:grid-cols-12 lg:gap-8">
+				<dt class="text-base/7 font-semibold text-gray-900 lg:col-span-5">
+					Как я отслежу что с клиентом?
+				</dt>
+				<dd class="mt-4 lg:col-span-7 lg:mt-0">
+					<p class="text-base/7 text-gray-600">
+						В личном кабинете сервиса будет вся информация о ваших клиентах, статусе сделок и
+						размере ожидаемых бонусов.
+					</p>
+				</dd>
+			</div>
+			<div class="py-8 first:pt-0 last:pb-0 lg:grid lg:grid-cols-12 lg:gap-8">
+				<dt class="text-base/7 font-semibold text-gray-900 lg:col-span-5">
+					Где гарантии, что я получу бонусы?
+				</dt>
+				<dd class="mt-4 lg:col-span-7 lg:mt-0">
+					<p class="text-base/7 text-gray-600">
+						Мы гарантируем выплату бонусов в течение 5 дней после закрытия сделки. Более того, мы
+						заинтересованы делать выплаты, так как это мотивирует вас на постоянную работу.
+					</p>
+				</dd>
+			</div>
+		</dl>
+	</div>
+</div>
