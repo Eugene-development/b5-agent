91844372d713eb10d6c0c6e48485f26220091e19
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-10-24 13:43:31 +0300

    c

--- src/routes/layout/header/UI/MobileMenu/index.svelte
+++ src/routes/layout/header/UI/MobileMenu/index.svelte
@@ -214,9 +214,9 @@
 							</button>
 
 							<button
-								onclick={() => navigate('/finances')}
+								onclick={() => navigate('/statistics')}
 								class="mb-2 flex w-full items-center overflow-hidden rounded-xl border border-slate-400/10 bg-gray-950 px-5 py-3.5 text-left text-base font-medium tracking-wide text-slate-200/90 shadow-sm transition-all duration-300 hover:border-indigo-400/30 hover:bg-indigo-500/15 hover:text-indigo-300 active:scale-98 {isActive(
-									'/finances'
+									'/statistics'
 								)
 									? 'border-indigo-400/40 bg-gradient-to-br from-indigo-500/25 to-indigo-600/25 text-indigo-400 shadow-md shadow-indigo-500/30'
 									: ''}"
@@ -231,10 +231,10 @@
 										stroke-linecap="round"
 										stroke-linejoin="round"
 										stroke-width="2"
-										d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1"
+										d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
 									/>
 								</svg>
-								Финансы
+								Статистика
 							</button>
 
 							<button
